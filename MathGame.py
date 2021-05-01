e50e810a1ce34c429630d136e3acb6b43cbc6f55
DESKTOP-DQLTBLK\janna <j.amhausend@gmail.com>
2021-05-01 20:59:05 +0200

    first version of a litte math based game

---
+++ MathGame.py
@@ -0,0 +1,20 @@
+#Improted the module random
+import random
+random.seed()
+
+#Added the 
+rdm1 = random.randint(1, 10)
+rdm2 = random.randint(1, 10)
+
+solution = rdm1 + rdm2
+print("The task:" , rdm1, "+" , rdm2,)
+
+print("Enter the solution:")
+
+x = input()
+number = int(x)
+
+if number == solution:
+    print(number, "is correct")
+else:
+    print("Solution: ", solution)
